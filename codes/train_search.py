640aa33f87cc06bde7dc7dd9d80cd0928426904e
invading <wzy634586400@163.com>
2021-04-01 02:16:05 +0800

    ettttt "1"
    This reverts commit 314153101923c2f27f770b5709b6e0af5479fc64.

--- codes/train_search.py
+++ codes/train_search.py
@@ -8,7 +8,7 @@ import utils
 import logging
 import argparse
 import torch.nn as nn
-# import torch.utils
+import torch.utils
 import torch.nn.functional as F
 import torchvision.datasets as dset
 import torch.backends.cudnn as cudnn
@@ -16,9 +16,53 @@ import torch.backends.cudnn as cudnn
 from torch.autograd import Variable
 from model_search import Network
 from architect import Architect
-# from tensorboard_logger import configure, log_value
+from tensorboard_logger import configure, log_value
 import pdb
 
+
+parser = argparse.ArgumentParser("cifar")
+parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
+parser.add_argument('--batch_size', type=int, default=64, help='batch size')
+parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate')
+parser.add_argument('--learning_rate_min', type=float, default=0.001, help='min learning rate')
+parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
+parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
+parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
+parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
+parser.add_argument('--epochs', type=int, default=50, help='num of training epochs')
+parser.add_argument('--init_channels', type=int, default=16, help='num of init channels')
+parser.add_argument('--layers', type=int, default=8, help='total number of layers')
+parser.add_argument('--model_path', type=str, default='saved_models', help='path to save the model')
+parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
+parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
+parser.add_argument('--drop_path_prob', type=float, default=0.3, help='drop path probability')
+parser.add_argument('--save', type=str, default='EXP', help='experiment name')
+parser.add_argument('--seed', type=int, default=2, help='random seed')
+parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
+parser.add_argument('--train_portion', type=float, default=0.5, help='portion of training data')
+parser.add_argument('--arch_learning_rate', type=float, default=3e-4, help='learning rate for arch encoding')
+parser.add_argument('--arch_weight_decay', type=float, default=1e-3, help='weight decay for arch encoding')
+parser.add_argument('--name', type=str, default="runs", help='name for log')
+parser.add_argument('--debug', action='store_true', default=False, help='debug or not')
+parser.add_argument('--greedy', type=float, default=0, help='explore and exploitation')
+parser.add_argument('--l2', type=float, default=0, help='additional l2 regularization for alphas')
+args = parser.parse_args()
+
+args.save = 'search-{}-{}'.format(args.save, time.strftime("%Y%m%d-%H%M%S"))
+if args.debug:
+  args.save += "_debug"
+utils.create_exp_dir(args.save, scripts_to_save=glob.glob('*.py'))
+
+log_format = '%(asctime)s %(message)s'
+logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+    format=log_format, datefmt='%m/%d %I:%M:%S %p')
+fh = logging.FileHandler(os.path.join(args.save, 'log.txt'))
+fh.setFormatter(logging.Formatter(log_format))
+logging.getLogger().addHandler(fh)
+configure(args.save + "/%s"%(args.name))
+
+
+CIFAR_CLASSES = 10
 
 
 def main():
@@ -57,12 +101,12 @@ def main():
   train_queue = torch.utils.data.DataLoader(
       train_data, batch_size=args.batch_size,
       sampler=torch.utils.data.sampler.SubsetRandomSampler(indices[:split]),
-      pin_memory=True, num_workers=3)
+      pin_memory=True, num_workers=2)
 
   valid_queue = torch.utils.data.DataLoader(
       train_data, batch_size=args.batch_size,
       sampler=torch.utils.data.sampler.SubsetRandomSampler(indices[split:num_train]),
-      pin_memory=True, num_workers=4)
+      pin_memory=True, num_workers=2)
 
   scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
         optimizer, float(args.epochs), eta_min=args.learning_rate_min)
@@ -70,10 +114,9 @@ def main():
   architect = Architect(model, args)
 
   for epoch in range(args.epochs):
-    # lr = scheduler.get_lr()[0]
-    lr = scheduler.get_last_lr()[0]
-
-    # log_value("lr", lr, epoch)
+    scheduler.step()
+    lr = scheduler.get_lr()[0]
+    log_value("lr", lr, epoch)
     logging.info('epoch %d lr %e', epoch, lr)
 
     genotype = model.genotype()
@@ -87,7 +130,7 @@ def main():
     logging.info("alphas_time %f ", alphas_time)
     logging.info("forward_time %f", forward_time)
     logging.info("backward_time %f", backward_time)
-    # log_value('train_acc', train_acc, epoch)
+    log_value('train_acc', train_acc, epoch)
     logging.info('train_acc %f', train_acc)
 
     # validation
@@ -95,12 +138,10 @@ def main():
     valid_acc, valid_obj = infer(valid_queue, model, criterion)
     end_time2 = time.time()
     logging.info("inference time %f", end_time2 - start_time2)
-    # log_value('valid_acc', valid_acc, epoch)
+    log_value('valid_acc', valid_acc, epoch)
     logging.info('valid_acc %f', valid_acc)
     logging.info('alphas_normal = %s', model.alphas_normal)
     logging.info('alphas_reduce = %s', model.alphas_reduce)
-
-    scheduler.step()
 
     utils.save(model, os.path.join(args.save, 'weights.pt'))
 
@@ -116,10 +157,10 @@ def train(train_queue, valid_queue, model, architect, criterion, optimizer, lr):
     model.train()
     n = input.size(0)
     input = Variable(input, requires_grad=False).cuda()
-    target = Variable(target, requires_grad=False).cuda()
+    target = Variable(target, requires_grad=False).cuda(async=True)
     input_search, target_search = next(iter(valid_queue))
     input_search = Variable(input_search, requires_grad=False).cuda()
-    target_search = Variable(target_search, requires_grad=False).cuda()
+    target_search = Variable(target_search, requires_grad=False).cuda(async=True)
     begin1 = time.time()
     architect.step(input, target, input_search, target_search, lr, optimizer)
     model.clip()
@@ -138,13 +179,12 @@ def train(train_queue, valid_queue, model, architect, criterion, optimizer, lr):
     end3 = time.time()
     backward_time += end3 - begin3
     model.restore()
-    # nn.utils.clip_grad_norm(model.parameters(), args.grad_clip)
-    nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip)
+    nn.utils.clip_grad_norm(model.parameters(), args.grad_clip)
     optimizer.step()
     prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
-    objs.update(loss.item(), n)
-    top1.update(prec1.item(), n)
-    top5.update(prec5.item(), n)
+    objs.update(loss.data[0], n)
+    top1.update(prec1.data[0], n)
+    top5.update(prec5.data[0], n)
 
     if step % args.report_freq == 0:
       logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
@@ -159,17 +199,17 @@ def infer(valid_queue, model, criterion):
   model.eval()
   model.binarization()
   for step, (input, target) in enumerate(valid_queue):
-    with torch.no_grad():
-      input = Variable(input).cuda()
-      target = Variable(target).cuda()
-      logits = model(input)
-      loss = criterion(logits, target)
+    input = Variable(input, volatile=True).cuda()
+    target = Variable(target, volatile=True).cuda(async=True)
+
+    logits = model(input)
+    loss = criterion(logits, target)
 
     prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
     n = input.size(0)
-    objs.update(loss.item(), n)
-    top1.update(prec1.item(), n)
-    top5.update(prec5.item(), n)
+    objs.update(loss.data[0], n)
+    top1.update(prec1.data[0], n)
+    top5.update(prec5.data[0], n)
 
     if step % args.report_freq == 0:
       logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
@@ -178,49 +218,5 @@ def infer(valid_queue, model, criterion):
 
 
 if __name__ == '__main__':
-  '''第一、 主脚本中的执行函数要放在 if _ name _ =='_ main '：代码块中，以防止多进程启动时主脚本不会再次运行（很可能会产生错误）'''
-
-  parser = argparse.ArgumentParser("cifar")
-  parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
-  parser.add_argument('--batch_size', type=int, default=16, help='batch size')
-  parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate')
-  parser.add_argument('--learning_rate_min', type=float, default=0.001, help='min learning rate')
-  parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
-  parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
-  parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
-  parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
-  parser.add_argument('--epochs', type=int, default=50, help='num of training epochs')
-  parser.add_argument('--init_channels', type=int, default=16, help='num of init channels')
-  parser.add_argument('--layers', type=int, default=8, help='total number of layers')
-  parser.add_argument('--model_path', type=str, default='saved_models', help='path to save the model')
-  parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
-  parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
-  parser.add_argument('--drop_path_prob', type=float, default=0.3, help='drop path probability')
-  parser.add_argument('--save', type=str, default='EXP', help='experiment name')
-  parser.add_argument('--seed', type=int, default=2, help='random seed')
-  parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
-  parser.add_argument('--train_portion', type=float, default=0.5, help='portion of training data')
-  parser.add_argument('--arch_learning_rate', type=float, default=3e-4, help='learning rate for arch encoding')
-  parser.add_argument('--arch_weight_decay', type=float, default=1e-3, help='weight decay for arch encoding')
-  parser.add_argument('--name', type=str, default="runs", help='name for log')
-  parser.add_argument('--debug', action='store_true', default=False, help='debug or not')
-  parser.add_argument('--greedy', type=float, default=0, help='explore and exploitation')
-  parser.add_argument('--l2', type=float, default=0, help='additional l2 regularization for alphas')
-  args = parser.parse_args()
-
-  args.save = 'result/search-{}-{}'.format(args.save, time.strftime("%Y%m%d-%H%M%S"))
-  if args.debug:
-    args.save += "_debug"
-  utils.create_exp_dir(args.save, scripts_to_save=glob.glob('*.py'))
-
-  log_format = '%(asctime)s %(message)s'
-  logging.basicConfig(stream=sys.stdout, level=logging.INFO,
-                      format=log_format, datefmt='%m/%d %I:%M:%S')
-  fh = logging.FileHandler(os.path.join(args.save, 'log.txt'))
-  fh.setFormatter(logging.Formatter(log_format))
-  logging.getLogger().addHandler(fh)
-  # configure(args.save + "/%s"%(args.name))
-
-  CIFAR_CLASSES = 10
   main() 
 
